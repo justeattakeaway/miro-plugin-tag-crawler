6831ad1832ba2173083ddce22eb42e6783e3d3e1
Sergej Popov <sergejpopov@outlook.com>
2022-01-23 22:29:59 +0000

    Widget deletion performance optimisation

--- common.ts
+++ common.ts
@@ -4,10 +4,7 @@ import { IMeta } from "./types";
 export async function clearLines() {
   const allLines = await miro.board.widgets.get({ type: "line" });
   const tagCrawlerLines = allLines.filter((l) => (l.metadata[config.clientId] as IMeta)?.isTagCrawlerLine);
-
-  for (let lineWidget of tagCrawlerLines) {
-    await miro.board.widgets.deleteById(lineWidget.id);
-  }
+  await miro.board.widgets.deleteById(tagCrawlerLines);
 }
 
 export async function getWidgetTags(widgetId: string) {
